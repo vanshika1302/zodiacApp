5000a4189d116f9664e73ebcc55c4fb57223b61a
Vanshika <mithi.vanshika@gmail.com>
2021-01-26 21:05:42 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/client'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('server is running on port by nodemon ' + port);
+});
